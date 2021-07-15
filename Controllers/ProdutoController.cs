7fc7cae60c2135b00c11360afb88a5f2020fb2a4
Erivan Oliveira <erivan.so@hotmail.com>
2021-07-15 15:25:49 -0300

    first commit

---
+++ Controllers/ProdutoController.cs
@@ -0,0 +1,70 @@
+﻿using WebApplication1.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
+
+namespace WebApplication1.Controllers
+{
+    public class ProdutoController : Controller
+    {
+
+        // GET: Produto
+        db_produtoStoreEntities1 dbObj = new db_produtoStoreEntities1();
+
+        public ActionResult Produto(tblProduto obj)
+        {
+                return View(obj);
+        }
+        [HttpPost]
+        public ActionResult AddProduto(tblProduto model)
+        {
+            tblProduto obj = new tblProduto();
+            if (ModelState.IsValid)
+            {
+                obj.Id = model.Id;
+                obj.Nome = model.Nome;
+                obj.Descricao = model.Descricao;
+                obj.Ativo = model.Ativo;
+                obj.Perecivel = model.Perecivel;
+                obj.Categoria = model.Categoria;
+
+                if (model.Id == 0)
+                {
+                    dbObj.tblProduto.Add(obj);
+                    dbObj.SaveChanges();
+                }
+                else
+                {
+                    dbObj.Entry(obj).State = EntityState.Modified;
+                    dbObj.SaveChanges();
+                }
+                
+            }
+            ModelState.Clear();
+
+                return View("Produto");
+        }
+        
+
+
+        public ActionResult ProdutoList()
+        {
+            var res = dbObj.tblProduto.ToList();
+            return View(res);
+        }
+
+        public ActionResult Deletar(int id)
+        {
+            var res = dbObj.tblProduto.Where(x => x.ID == id).First();
+            dbObj.tblProduto.Remove(res);
+            dbObj.SaveChanges();
+
+            var list = dbObj.tblProduto.ToList();
+
+            return View("ProdutoList", list);
+        }
+    }
+}
---
+++ Controllers/db_produtoStoreEntities1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WebApplication1.Controllers
+{
+    internal class db_produtoStoreEntities1
+    {
+        internal object tblProduto;
+
+        internal void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
